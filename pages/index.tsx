2202893ab1c4310d3d7c038c4815ea2aeca311e0
georgemiraclee <georgemiracle2019@gmail.com>
2024-01-23 16:12:38 +0700

    change logo

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ import { FiFigma, FiCode, FiBox } from 'react-icons/fi'
 const Home = () => {
   return (
     <>
-      <PageTemplate title="Home - Collosal">
+      <PageTemplate title="Home - Levago">
         {/* Banner Section */}
         <section
           className="flex flex-col gap-10 items-center"
