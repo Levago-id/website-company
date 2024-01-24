7a9c4b3c2c750fee1079e5a39a2296714920862f
georgemiraclee <georgemiracle2019@gmail.com>
2024-01-24 22:13:12 +0700

    change color web and navbar

--- components/atoms/NavLink/index.tsx
+++ components/atoms/NavLink/index.tsx
@@ -1,28 +1,47 @@
-import Link, { LinkProps } from 'next/link'
-import React from 'react'
-import { useRouter } from 'next/router'
-import useMobileDeviceDetection from 'hooks/useMobileDetection'
+import Link from 'next/link';
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import useMobileDeviceDetection from 'hooks/useMobileDetection';
+
 interface Props {
-  href: string
-  value: string
-  canActive?: boolean
-}
-const NavLink = ({ href, value, canActive = false }: Props) => {
-  const router = useRouter()
-  const isMobile = useMobileDeviceDetection()
-  return (
-      <Link href={href} passHref>
-        <p
-          className={`text-base text-white select-none hover:text-white ${
-            router.pathname == href && canActive
-              ? 'font-semibold'
-              : 'text-opacity-80'
-          } ${isMobile ? 'cursor-default' : 'cursor-pointer'}`}
-        >
-          {value}
-        </p>
-      </Link>
-  )
+  href: string;
+  value: string;
+  canActive?: boolean;
 }
 
-export default NavLink
+const NavLink = ({ href, value, canActive = false }: Props) => {
+  const router = useRouter();
+  const isMobile = useMobileDeviceDetection();
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollThreshold = 100;
+      const currentScroll = window.scrollY;
+
+      setIsScrolled(currentScroll > scrollThreshold);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <Link href={href} passHref>
+      <p
+        className={`select-none hover:text-white ${
+          (router.pathname === href && canActive) || isScrolled
+            ? 'font-semibold text-blue-900'
+            : 'text-opacity-80'
+        } ${isMobile ? 'cursor-pointer text-blue-900' : 'cursor-pointer text-blue-1000 font-semibold'}`}
+      >
+        {value}
+      </p>
+    </Link>
+  );
+};
+
+export default NavLink;
--- pages/index.tsx
+++ pages/index.tsx
@@ -22,9 +22,9 @@ const Home = () => {
         >
           <div className="w-10/12 md:w-8/12 text-center">
             <PageSentence
-              title="We Design. We Develop. We Ship. In The Same Day."
+              title="Digitalize Your Business With Levago."
               description="We are committed to not making clients wait. We will deliver the work as quickly as possible. Even on the same day. Even so, we do not reduce the quality of our work."
-              badge="CLIENT-DEVELOPMENT-DRIVEN"
+              badge="LEVAGO INDONESIA"
             />
           </div>
           <div className="flex flex-col gap-6 sm:flex-row w-full sm:w-fit">
